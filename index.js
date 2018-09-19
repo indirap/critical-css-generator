ff00b4b983364838861e5d257ebf2bc672d2b8e5
Indira Pranabudi <ipranabudi@vistaprint.com>
2018-09-19 09:35:09 -0400

    MOB-83 Add option to get critical CSS for just viewport, add configurable css selector filter to options

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 const fs = require('fs');
 const CleanCSS = require('clean-css');
+const cssSelectorExtract = require('css-selector-extract');
 
 exports.generate = (async (opts) => {
     const options = Object.assign({
         deviceName: 'Pixel 2',
         waitFor: 20000,
-        path: 'critical.css'
+        path: 'critical.css',
+        cssSelectorFilter: []
     }, opts);
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.emulate(devices[options.deviceName]);
-
-    await Promise.all([
-        page.coverage.startCSSCoverage()
-    ]);
+    await page.coverage.startCSSCoverage();
 
     await page.goto(options.url, {
         timeout: 100000
@@ -29,15 +28,80 @@ exports.generate = (async (opts) => {
         page.coverage.stopCSSCoverage()
     ]);
 
-    let cssString = "";
-    for (const entry of cssCoverage) {        
+    let criticalCss = "";
+    for (const entry of cssCoverage) {
         entry.ranges.forEach((range) => {
-            cssString += entry.text.substring(range.start, range.end) + "\n";
+            criticalCss += entry.text.substring(range.start, range.end) + "\n";
         });
     }
-    cssString += "body{width:100vw}";
+    criticalCss += "body{width:100vw}";
 
-    const minifiedCss = new CleanCSS({ compatibility: '*' }).minify(cssString).styles;
+    if (options.viewport) {
+        // Get all CSS selectors from minified string
+        const cssSelectorRegex = /(\.)([\w-.\: ]+)/g; // TODO narrow down regex to not catch width and transition measurements (eg .25em)
+        const cssSelectors = criticalCss.match(cssSelectorRegex).filter((cssSelector, index, self) => self.indexOf(cssSelector) === index).map((cssSelector) => cssSelector.trim());
+
+        // Get all elements with CSS selectors above
+        const elementsBySelector = {};
+        let promises = [];
+        cssSelectors.forEach((cssSelector) => {
+            promises.push(
+                page
+                    .$$(cssSelector)
+                    .then((elements) => {
+                        elementsBySelector[cssSelector] = elements;
+                    })
+                    .catch((err) => {
+                        // console.error(err); // so noisy, pls just shut up
+                    })
+            )
+        });
+        await Promise.all(promises);
+
+        // For each selector, check if they have elements in the viewport
+        promises = [];
+        const criticalCssSelectors = [];
+        Object.keys(elementsBySelector).forEach((cssSelector) => {
+            elementsBySelector[cssSelector].forEach((element) => {
+                promises.push(
+                    element
+                        .isIntersectingViewport()
+                        .then((isIntersectingViewport) => {
+                            if (isIntersectingViewport && !criticalCssSelectors.includes(cssSelector)) {
+                                criticalCssSelectors.push(cssSelector);
+                            }
+                        })
+                        .catch((err) => {
+                            console.error(err);
+                        })
+                );
+            });
+        });
+        await Promise.all(promises);
+
+        // Just add all that have "header" in the name
+        Object.keys(elementsBySelector).forEach((cssSelector) => {
+            if (options.cssSelectorFilter.length > 0) {
+                if (options.cssSelectorFilter.find((filter) => !criticalCssSelectors.includes(cssSelector) && filter.test(cssSelector))) {
+                    criticalCssSelectors.push(cssSelector.trim());
+                }
+            }
+        });
+
+        const cssSelectorExtractOptions = {
+            css: criticalCss,
+            filters: criticalCssSelectors
+        };
+        await cssSelectorExtract.process(cssSelectorExtractOptions)
+            .then((extractedCss) => {
+                criticalCss = extractedCss;
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    const minifiedCss = new CleanCSS({ compatibility: '*' }).minify(criticalCss).styles;
     fs.writeFileSync(options.path, minifiedCss, 'utf8');
     
     await browser.close();
--- inject-critical-styles.js
+++ inject-critical-styles.js
@@ -1,5 +1,20 @@
-var stuff = document.createElement("style");
-var node = document.createTextNode('<insert css here>');
-stuff.appendChild(node);
+// Remove all the existing stylesheets
+var stylesheets = document.querySelectorAll('link[rel="stylesheet"]');
+stylesheets.forEach((stylesheet) => {
+    stylesheet.remove();
+});
 
-document.head.appendChild(stuff);
+// Add the critical CSS
+var criticalStyles = document.createElement("style");
+var node = document.createTextNode('<insert critical css>');
+criticalStyles.appendChild(node);
+document.head.appendChild(criticalStyles);
+
+// After load, re-add all the existing stylesheets and remove critical CSS
+window.addEventListener("load", (event) => {
+    stylesheets.forEach((stylesheet) => {
+        document.head.appendChild(stylesheet);
+    });
+
+    criticalStyles.remove();
+});
